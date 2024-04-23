a91a55b0517e9b4348fab6a0e43852f444eedd7c
Ayush Soni <ayushsoni1010.work@gmail.com>
2024-04-24 00:18:39 +0530

    feat: base button

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { ButtonProps } from "../../types/button";
+
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, type = "primary", ...props }, ref) => {
+    const variants: any = {
+      primary: "gradient text-white",
+      secondary: "",
+      link: "bg-white",
+    };
+
+    return (
+      <button
+        className={`inline-flex items-center justify-center rounded-md text-base font-medium h-12 px-8 py-2 ${variants[type]} ${className}`}
+        ref={ref}
+        {...props}
+      ></button>
+    );
+  }
+);
+Button.displayName = "Button";
+
+export default Button;
